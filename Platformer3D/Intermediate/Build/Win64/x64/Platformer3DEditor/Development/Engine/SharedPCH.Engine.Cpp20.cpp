c089434e605469d2b9d06a7b5b7f18e92c71c149
scottmcpherson99 <scottmcpherson56@gmail.com>
2024-04-22 16:24:37 +0100

    Added Blank Project
    - Added blank project and git ignore

---
+++ Platformer3D/Intermediate/Build/Win64/x64/Platformer3DEditor/Development/Engine/SharedPCH.Engine.Cpp20.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.36.32545
+#include "C:/Users/helen.KEELWORKS/Documents/Unreal Projects/Platformer3D/Intermediate/Build/Win64/x64/Platformer3DEditor/Development/Engine/SharedPCH.Engine.Cpp20.h"
---
+++ Platformer3D/Intermediate/Build/Win64/x64/Platformer3DEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
@@ -0,0 +1,3 @@
+// PCH for Runtime/Slate/Public/SlateSharedPCH.h
+#include "C:/Users/helen.KEELWORKS/Documents/Unreal Projects/Platformer3D/Intermediate/Build/Win64/x64/Platformer3DEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
+#include "Runtime/Slate/Public/SlateSharedPCH.h"
